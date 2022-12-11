a0f6642d0fd70435ff60db657d9ce1083c93fe33
Andrew Barbosa Deras <andrewderas7@gmail.com>
2022-12-11 16:42:41 -0300

    11dez

--- curso-de-js-roger-melo/exercicio-25/app.js
+++ curso-de-js-roger-melo/exercicio-25/app.js
@@ -18,7 +18,9 @@ const months = [
   'Outubro',
   'Novembro',
   'Dezembro'
-]
+];
+
+console.log(months.indexOf('Fevereiro'));
 
 /*
   02
@@ -27,38 +29,46 @@ const months = [
   - Exiba o objeto no console.
 */
 
+const present = new Date();
+
+console.log({ present });
+
 /*
   03
 
   - Baseado no objeto que você acabou de criar, exiba o ano atual no console.
 */
-
+console.log(present.getFullYear());
 /*
   04
 
   - Crie um objeto de data que represente um momento passado;
   - Exiba o objeto no console.
 */
-
+const past = new Date('January 22 1998 21:00:00');
+console.log({ past });
 /*
   05
 
   - Exiba, no console, a hora do objeto que você acabou de criar.
 */
-
+console.log(past.getHours());
 /*
   06
 
   - Crie um objeto de data que represente um momento futuro;
   - Exiba o objeto no console.
 */
-
+const future = new Date('January 22 2023 00:10:00');
+console.log(future);
 /*
   07
 
   - Exiba no console a quantidade de dias entre o momento futuro e o passado.
 */
-
+const difference = future.getTime() - past.getTime();
+const differenceInDays = Math.round(difference / 1000 / 60 / 60 / 24)
+console.log(differenceInDays);
 /*
   08
   
--- exercicios/14-exercicio/main.js
+++ exercicios/14-exercicio/main.js
@@ -10,7 +10,7 @@ const ulMenores = document.querySelector('.menores-de-idade');
 const menoresCol = document.querySelector('.menoresCol');
 const maioresCol = document.querySelector('.maioresCol');
 
-let pessoas = []
+let pessoas = [];
 
 btn.addEventListener('click', () => {
     pessoas.push({ nome: nome.value, idade: idade.value });
@@ -23,38 +23,38 @@ btnMaiores.addEventListener('click', () => {
     let maiores = pessoas.reduce((maiores, pessoa) => {
         if (pessoa.idade >= 18) {
             maiores.push(pessoa)
-        }
-        return maiores
+        };
+        return maiores;
     }, []);
 
     for (let i = 0; i < maiores.length; i++) {
-        const person = maiores[i];
-        maioresCol.textContent = `${maiores.length} maiores de idade.`
+        const pessoa = maiores[i];
+        maioresCol.textContent = `${maiores.length} maiores de idade.`;
         ulMaiores.innerHTML += `
         <li class="list-group-item list-group-item-success">
-            ${person.nome}
-        </li>`
-    }
+           Nome: ${pessoa.nome} - Idade: ${pessoa.idade}
+        </li>`;
+    };
 });
 
 btnMenores.addEventListener('click', () => {
     let menores = pessoas.reduce((menores, pessoa) => {
         if (pessoa.idade < 18) {
-            menores.push(pessoa)
+            menores.push(pessoa);
         }
-        return menores
+        return menores;
     }, []);
 
     for (let i = 0; i < menores.length; i++) {
-        const person = menores[i];
-        menoresCol.textContent = `${menores.length} menores de idade.`
+        const pessoa = menores[i];
+        menoresCol.textContent = `${menores.length} menores de idade.`;
         ulMenores.innerHTML += `
         <li class="list-group-item list-group-item-danger">
-            ${person.nome}
-        </li>`
-    }
+        Nome: ${pessoa.nome} - Idade: ${pessoa.idade}
+        </li>`;
+    };
 
-})
+});
 
 
 
