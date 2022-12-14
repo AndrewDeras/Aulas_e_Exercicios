8b68d820525586ac44d2afa8b5d2a0a5a5659c37
Andrew Barbosa Deras <andrewderas7@gmail.com>
2022-12-14 11:40:54 -0300

    14dez/2

--- exercicios/03-exercicio/main.js
+++ exercicios/03-exercicio/main.js
@@ -1,1 +1,63 @@
-console.log('run');
+const produtos = [
+    {
+        nome: 'TvLG',
+        preco: 3000,
+    },
+    {
+        nome: 'TvSamsung',
+        preco: 2500,
+    },
+    {
+        nome: 'Fogao',
+        preco: 900,
+    },
+    {
+        nome: 'Microondas',
+        preco: 750,
+    },
+    {
+        nome: 'Geladeira',
+        preco: 1000,
+    },
+];
+
+class Produto {
+
+    constructor(id, nome, preco) {
+        this.setId(id);
+        this.setNome(nome);
+        this.setPreco(preco);
+        this.setImg();
+    }
+
+    setId(id) {
+        this.id = id;
+    }
+
+    getId() {
+        return this.id;
+    }
+
+    setNome(nome) {
+        this.nome = nome;
+    }
+    getNome() {
+        return this.nome;
+    }
+
+    setPreco(preco) {
+        this.preco = preco;
+    }
+
+    getPreco() {
+        return this.preco;
+    }
+
+    setImg() {
+        this.img = "./img/" + this.getNome().toLowerCase() + ".jpg";
+    }
+
+    getImg() {
+        return this.img;
+    }
+};
