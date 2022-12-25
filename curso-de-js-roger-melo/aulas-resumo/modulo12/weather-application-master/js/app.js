9684a0ae0ad4f7910ee6c8ed657e5debaf6c575e
andrewDeras <andrewderas7@gmail.com>
2022-12-24 22:00:53 -0300

    24dez/2

--- curso-de-js-roger-melo/aulas-resumo/modulo12/weather-application-master/js/app.js
+++ curso-de-js-roger-melo/aulas-resumo/modulo12/weather-application-master/js/app.js
@@ -5,23 +5,31 @@ const cityWeatherContainer = document.querySelector('[data-js="city-weather"]');
 const cityTemperatureContainer = document.querySelector('[data-js="city-temperature"]');
 
 const cityCard = document.querySelector('[data-js="city-card"]')
+let timeImg = document.querySelector('[data-js="time"]');
+let weatherIconContainer = document.querySelector('[data-js="time-icon"]')
 
 
 cityForm.addEventListener('submit', async event => {
     event.preventDefault();
     const inputValue = event.target.city.value;
     const [{ Key, LocalizedName }] = await getCityData(inputValue);
-    const [{ WeatherText, Temperature }] = await getCityWeather(Key);
+    const [{ WeatherText, IsDayTime, Temperature, WeatherIcon }] = await getCityWeather(Key);
+
+    const timeIcon = `<img src="./src/icons/${WeatherIcon}.svg"/>`
 
     if (cityCard.classList.contains('d-none')) {
         cityCard.classList.remove('d-none')
     }
-
+    if (IsDayTime) {
+        timeImg.src = './/src/day.svg';
+    } else {
+        timeImg.src = './/src/night.svg';
+    }
 
     cityNameContainer.textContent = LocalizedName
     cityWeatherContainer.textContent = WeatherText
     cityTemperatureContainer.textContent = Temperature.Metric.Value
 
-    console.log(WeatherText, Temperature);
+    weatherIconContainer.innerHTML = timeIcon
     cityForm.reset();
 })
